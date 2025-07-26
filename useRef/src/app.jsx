256e47bad69c646e0b6a8e21f00bd5d1a11b9026
Aman kureshi <amankureshi78692@gmail.com>
2025-07-26 22:57:58 +0530

    added app class in main.jsx

--- useRef/src/app.jsx
+++ useRef/src/app.jsx
@@ -1,6 +1,9 @@
+import { useRef } from "preact/hooks";
 import "./app.css";
 
 export function App() {
+  const inputRef = useRef(null);
+
   const handleFocus = () => {
     inputRef.current.focus();
   };
@@ -9,6 +12,7 @@ export function App() {
     <>
       <h1>Focus input with useRef</h1>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Type something...."
         style={{ padding: "10px", fontSize: "1rem" }}
